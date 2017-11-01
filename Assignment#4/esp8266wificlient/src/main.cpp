f7b61ac0df90949631ab64c9412bce15a3db6b68
Yoshimichi Nakatsuka <y.nakatsuka443@gmail.com>
2017-11-01 15:16:42 -0700

    Modified ESP code to send accelerometer sensor data

---
+++ Assignment#4/esp8266wificlient/src/main.cpp
@@ -0,0 +1,109 @@
+#include "Arduino.h"
+#include "ESP8266WiFi.h"
+#include "ESP8266HTTPClient.h"
+//#include "secret.h"
+#include "MAX30105.h"
+#include "Wire.h"
+#include "SparkFunLIS3DH.h"
+#include "SPI.h"
+
+const char* ssid = "UCInet Mobile Access";
+String base_host = "http://169.234.50.128/arduino_simple.php";
+int counter = 0;
+int loop_counter = 0;
+const int counter_limit = 3000;
+const int send_counter = 100;
+
+float x[send_counter+1];
+float y[send_counter+1];
+float z[send_counter+1];
+
+unsigned long start_time = 0;
+
+// Create "secret.h" for "ssid" and "host"
+
+HTTPClient http;
+//MAX30105 MAX;
+LIS3DH myIMU(SPI_MODE, 5); // constructed with parameters for SPI and cs pin number
+
+void sendtoWebServer(int c){
+
+    String body_data = "data=";
+    for (int i = 0; i < send_counter; i++) {
+      body_data = body_data + String(c + i) + "," + String(x[i]) + "," + String(y[i]) + "," + String(z[i]) + "\n\r";
+    }
+    Serial.println(body_data);
+    Serial.println(counter);
+    
+    http.begin(base_host.c_str());
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+    int httpCode = http.POST(body_data);
+
+    if (httpCode > 0) {
+        Serial.print("POST code: ");
+        Serial.println(httpCode);
+        if (httpCode == HTTP_CODE_OK) {
+            Serial.print(base_host);
+            Serial.println(" succeeded");
+        }
+    } else {
+        Serial.print("POST failed, error: ");
+        Serial.println(http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println();
+
+    // Show MAC Address
+    Serial.print("MAC Address: ");
+    Serial.println(WiFi.macAddress());
+    Serial.println();
+
+    // Connect to WiFi and show IP Address
+    Serial.println("Connecting to WiFi...");
+    //WiFi.begin(ssid, password);
+    WiFi.begin(ssid);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println();
+    Serial.println("WiFi connected");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+    
+    //Call .begin() to configure the IMU
+    myIMU.begin();
+
+    Serial.println();
+
+    start_time = millis();
+}
+
+void loop() {   
+    if (loop_counter < counter_limit) {
+      x[counter] = myIMU.readFloatAccelX();
+      y[counter] = myIMU.readFloatAccelY();
+      z[counter] = myIMU.readFloatAccelZ();
+      counter = counter + 1;
+      //if ( counter == send_counter) {
+      if ((counter % send_counter) == 0) {
+        Serial.println(millis() - start_time);
+        sendtoWebServer(loop_counter - send_counter);
+        //Serial.println("reached");
+        counter = 0;
+      }
+    }
+    /*if ( counter == send_counter) {
+        sendtoWebServer();
+    }*/
+    //Serial.println(counter);
+    loop_counter = loop_counter + 1;
+}
